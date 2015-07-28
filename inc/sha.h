f89bd47d26ee2f83818cfc512b444dacdaf23773
glb <gleb.billig@gmail.com>
2015-07-27 20:58:26 -0400

    Added sigma SHA256 helper functions

--- inc/sha.h
+++ inc/sha.h
@@ -18,6 +18,9 @@ uint32_t Maj(uint32_t x, uint32_t y, uint32_t z);
 uint32_t bit_rotation(uint32_t x, int shift);
 uint32_t bit_shift(uint32_t x, int shift);
 
-
+uint32_t big_sigma0(uint32_t x);
+uint32_t big_sigma1(uint32_t x);
+uint32_t small_sigma0(uint32_t x);
+uint32_t small_sigma1(uint32_t x);
 
 #endif /* INC_SHA_H_ */
--- src/sha_helper.c
+++ src/sha_helper.c
@@ -10,6 +10,7 @@
 #include <stdint.h>
 
 #include "../inc/aes.h"
+#include "../inc/sha.h"
 
 uint32_t Ch(uint32_t x, uint32_t y, uint32_t z) {
 
@@ -45,5 +46,25 @@ uint32_t bit_shift(uint32_t x, int shift) {
 		result = result & 0x7FFFFFFF;
 	}
 
+	return result;
+}
+
+uint32_t big_sigma0(uint32_t x) {
+	uint32_t result = bit_rotation(x, 2) + bit_rotation(x, 13) + bit_rotation(x, 22);
+	return result;
+}
+
+uint32_t big_sigma1(uint32_t x) {
+	uint32_t result = bit_rotation(x, 6) + bit_rotation(x, 11) + bit_rotation(x, 25);
+	return result;
+}
+
+uint32_t small_sigma0(uint32_t x) {
+	uint32_t result = bit_rotation(x, 7) + bit_rotation(x, 18) + bit_shift(x, 3);
+	return result;
+}
+
+uint32_t small_sigma1(uint32_t x) {
+	uint32_t result = bit_rotation(x, 17) + bit_rotation(x, 19) + bit_shift(x, 10);
 	return result;
 }
